1eac81aa5a276c809325f31b359256a3f6762928
SoumiaDelmi <150604910+SoumiaDelmi@users.noreply.github.com>
2024-03-01 16:36:03 +0100

    Ajout des modeles de produit

--- service-reapprovisionnement/service-reapprovisionnement/Models/Fournisseur.cs
+++ service-reapprovisionnement/service-reapprovisionnement/Models/Fournisseur.cs
@@ -1,9 +1,10 @@
 namespace Reapprovisionnement.Models;
 
-public class Fournissuer
+public class Fournisseur
 {
-    //public Guid Id { get; set; }
+    public Guid Id { get; set; }
     public int idFournissuer { get; set; }
+    public String nom { get; set; }
     public String email { get; set; }
     public String raison_sociel { get; set; }
     public List<Produit> Produits { get; set; }
--- service-reapprovisionnement/service-reapprovisionnement/Models/Produit.cs
+++ service-reapprovisionnement/service-reapprovisionnement/Models/Produit.cs
@@ -4,10 +4,4 @@ public class Produit
 {
     public double PrixUnitaireFournisseur { get; set; }
     public int IdProduitCatalogue { get; set; } 
-    public string Libelle { get; set; }
-    public string Description { get; set; }
-    public List<string> Couleurs { get; set; }
-    public string Sexe { get; set; }
-    public string Taille { get; set; }
-    public string ImageUrl { get; set; }
 }
